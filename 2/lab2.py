e0f1d7fe8ee9e7caa13cad8ce781c4e8bf9f5130
Oleg <gagileveu@mail.ru>
2023-02-10 15:50:12 +0500

    III

--- 2/lab2.py
+++ 2/lab2.py
@@ -2,8 +2,8 @@ print("Способ 1")
 for i in range (1,1001):
     g = len(str(i))
     if i == (i ** 2) % 10 ** g:
-        print(i)
+        print(i, "=", i**2)
 print("Способ 2")
 for i in range (1,1001):
     if str(i) == str(i**2)[-len(str(i)):]:
-        print(i)
+        print(i, "=", i**2)
