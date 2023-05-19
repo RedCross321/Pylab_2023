0a68c65a6539e57b7602c0a464d9470d0ce568d7
Oleg <gagileveu@mail.ru>
2023-05-19 19:23:45 +0500

    10+-

--- 10/10.py
+++ 10/10.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import room
+import docx
 
 
 root = Tk()
@@ -12,8 +13,8 @@ ttk.Label(text="Высота помещения в м3").place(x=100, y=80)
 ttk.Label(text="Температура воздуха вне помещения в °С").place(x=100, y=110)
 ttk.Label(text="Температура воздуха в помещения в °С").place(x=100, y=140)
 ttk.Label(text="Коэффициент тепловых потерь").place(x=100, y=170)
-ttk.Label(text="Сколько комнат в этой квартире").place(x=700, y=200)
-ttk.Label(text="Сколько квартир в этом доме").place(x=700, y=230)
+ttk.Label(text="Сколько комнат в этой квартире").place(x=700, y=500)
+ttk.Label(text="Сколько квартир в этом доме").place(x=700, y=530)
 ttk.Label(text="РЕЗУЛЬТАТЫ ПОДСЧЕТОВ").place(x=800, y=300)
 ttk.Label(text="Общая площадь помещения:").place(x=700, y=330)
 ttk.Label(text="Тепловой мощности для обогрева помещения(кВт):").place(x=700, y=360)
@@ -45,42 +46,37 @@ Ekof = ttk.Entry(width=50)
 Ekof.place(x=700, y=170)
 
 Rooms = ttk.Entry(width=20)
-Rooms.place(x=900, y=200)
+Rooms.place(x=935, y=500)
 
 Apartmens = ttk.Entry(width=20)
-Apartmens.place(x=900, y=230)
-
-N = 0
-
-def cpcp():
-    N = int(Rooms.get())
-    Elength.delete(0, END)
-    Ewidth.delete(0, END)
-    Eheight.delete(0, END)
-    Etemp1.delete(0, END)
-    Etemp2.delete(0, END)
-    Ekof.delete(0, END)
+Apartmens.place(x=935, y=530)
 
 def pcpc():
-    N = int(Apartmens.get())
-    Elength.delete(0, END)
-    Ewidth.delete(0, END)
-    Eheight.delete(0, END)
-    Etemp1.delete(0, END)
-    Etemp2.delete(0, END)
-    Ekof.delete(0, END)
+    pass
+def cpcp():
+    pass
 
 
 def click_button():
     RezS["text"], RezP["text"] = room.schet(float(Elength.get()), float(Ewidth.get()), float(Eheight.get()), int(Etemp1.get()),int(Etemp2.get()),float(Ekof.get()))
 
+def save():
+    mydoc = docx.Document()
+    RS = RezS["text"]
+    RP = RezP["text"]
+    mydoc.add_paragraph(f"Общая площадь помещения: {RS}\n\nТепловой мощности для обогрева помещения(кВт): {RP}")
+    mydoc.save("./Otchet.docx")
+
 btn = ttk.Button(text="Посчитать", command=click_button)
 btn.place(x=700, y=400)
 
 btn1 = ttk.Button(text="ок", command=cpcp)
-btn1.place(x=1100, y=200)
+btn1.place(x=1110, y=500)
 
 btn2 = ttk.Button(text="окей", command=pcpc)
-btn2.place(x=1100, y=230)
+btn2.place(x=1110, y=530)
+
+btn2 = ttk.Button(text="Сохранить результаты в отчет", command=save)
+btn2.place(x=900, y=400)
 
 root.mainloop()
